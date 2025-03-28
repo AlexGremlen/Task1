9c14ff1e033df8b307aa2ecd68e1d3259ba4b716
Alex Gremlen <alexgremlen@yandex.ru>
2025-03-28 14:53:43 +0300

    добавляем

---
+++ src/task1/Main.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class Main {
+    public static void main(String[]args) {
+        // Проверяем, что переданы два аргумента
+        if (args.length != 2) {
+            System.out.println("Пожалуйста, укажите два числа: n и m");
+
+        }
+    }
+}
