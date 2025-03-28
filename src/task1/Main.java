822438736374f25f5a420becbb9818a91cafc6e3
Alex Gremlen <Alexgremlen@yandex.ru>
2025-03-28 15:24:31 +0300

    проверяю что полученные числа отображаються и что получилось преобразовать строки в число

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,11 +1,19 @@
 package task1;
 
-public class Main {
+public class Main { //мой класс main
     public static void main(String[]args) {
-        // Проверяем, что переданы два аргумента
+        // Проверяем, что переданы два аргумента, если не 2 числа будет сообщение ошибки
         if (args.length != 2) {
             System.out.println("Пожалуйста, укажите два числа: n и m");
-
+            return; //завершим програму если не 2 числа
         }
+        int n = 0; // это будет n
+        int m = 0; // это соответственно m
+
+        n = Integer.parseInt(args[0]);
+        m = Integer.parseInt(args[1]);
+
+        System.out.println(n);
+        System.out.println(m);
     }
 }
